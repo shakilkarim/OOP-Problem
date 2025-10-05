2efbba5b2fb2f57380bf51fc2af6e345ffbd64a4
Shakilkarim <2736425-Shakilkarim@users.noreply.replit.com>
2025-10-05 04:44:34 +0000

    this is first change

--- main.cs
+++ main.cs
@@ -1,11 +1,15 @@
 using System;
 
-
 class Student
 {
+
     public string name;
     public int age;
-
+    public Student(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
     public void showInfo()
     {
         Console.WriteLine("Name: {0}, Age: {1}", name, age);
@@ -13,18 +17,22 @@ class Student
 
 }
 
+// class Teacher
+// {
+//     public string name;
+//     public string subject;
 
-class program
+//     public void showInfo()
+//     {
+//         Console.WriteLine("Name: {0}, Subject: {1}", name, subject);
+//     }
+// }
+
+class Program
 {
     public static void Main()
     {
-        Student student1 = new Student();
-        student1.name = "John";
-        student1.age = 20;
-
-        Student student2 = new Student();
-        student2.name = "Jane";
-        student2.age = 21;
+        Student student1 = new Student("shakil", 20);
 
         student1.showInfo();
     }
